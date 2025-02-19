bc2f783518843e239389872ddadf928f72e648f8
srushti toli <srushtitoli05@gmail.com>
2025-02-19 16:00:48 +0530

    done

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,29 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    username:{
-        type:String,
-        required:true
-    },
-    token:{
-        type:String,
-        required:true
-    }
-})
-const User=mongoose.model('User',userSchema);
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  token: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  cart: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Cart",
+  },
+});
 
-module.exports={User};
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
